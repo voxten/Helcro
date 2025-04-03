e903d22ff98f1349faff9185085cf617e7a71432
voxten <52979294+voxten@users.noreply.github.com>
2025-04-03 14:03:50 +0200

    Fixed bug with private class methods

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['module:react-native-dotenv'],
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+        ['@babel/plugin-transform-private-methods', { loose: true }],
+        ['@babel/plugin-transform-class-properties', { loose: true }],
+        ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+        'module:react-native-dotenv',
+    ],
 };
