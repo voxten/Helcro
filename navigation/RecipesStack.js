e18088334418003fdd281a120a636c887b17fb49
Bartosz <MaciejewskiBartosz123@wp.pl>
2025-04-08 21:12:04 +0200

    Update RecipesStack.js

--- navigation/RecipesStack.js
+++ navigation/RecipesStack.js
@@ -1,15 +1,39 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import RecipesList from "../components/recipes/RecipesList";
-import RecipeDetail from "../components/recipes/RecipeDetail"; // Importujemy ekran RecipeDetail
+import RecipeDetail from "../components/recipes/RecipeDetail";
+import RecipesAdd from "../components/recipes/RecipesAdd";
 
 const Stack = createStackNavigator();
 
 export default function RecipesStack() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="RecipesList" component={RecipesList} options={{ title: "Recipes" }} />
-            <Stack.Screen name="RecipeDetail" component={RecipeDetail} options={{ title: "Recipe Detail" }} />
+            <Stack.Screen 
+                name="RecipesList" 
+                component={RecipesList} 
+                options={{ 
+                    headerShown: false // This hides the header for RecipesList
+                }} 
+            />
+            <Stack.Screen 
+                name="RecipeDetail" 
+                component={RecipeDetail} 
+                options={{ 
+                    title: "Recipe Detail",
+                    headerStyle: { backgroundColor: 'brown' },
+                    headerTintColor: 'white' 
+                }} 
+            />
+            <Stack.Screen 
+                name="RecipesAdd" 
+                component={RecipesAdd} 
+                options={{ 
+                    title: 'Add Recipe',
+                    headerStyle: { backgroundColor: 'brown' },
+                    headerTintColor: 'white'
+                }} 
+            />
         </Stack.Navigator>
     );
 }
