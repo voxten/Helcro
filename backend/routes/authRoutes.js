e5538102e66a93da1ed841a8bf5d289e1b11f7b4
20510 <20510@student.ans-elblag.pl>
2025-05-09 08:55:09 +0200

    Ready to build

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/AuthController');
-const router = express.Router();
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
