6f4ec2d4ba5ae1e096366c1c54b4b698c80c05d6
20510 <20510@student.ans-elblag.pl>
2025-05-20 14:47:17 +0200

    Backend

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: ['module:@react-native/babel-preset'],
-    plugins: [
-        ['@babel/plugin-transform-private-methods', { loose: true }],
-        ['@babel/plugin-transform-class-properties', { loose: true }],
-        ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-        'module:react-native-dotenv',
-    ],
-};
